5dc5a5a260f56873fb164641a372d0622c889350
julcsi333 <jankojulia333@gmail.com>
2022-05-17 16:31:30 +0200

    Fixed asteroid blasting robot bug
    Exploratory testing bug #1000

--- src/main/java/Asteroid.java
+++ src/main/java/Asteroid.java
@@ -102,8 +102,9 @@ public class Asteroid implements INeighbour {
      * Ezután a sun-nak meghívja a removeAsteroid metódusát magával paraméterként.
      */
     public void radioactiveBlast() {
-    	for (int i = 0; i < travellers.size(); i++)
-    		travellers.get(i).hitByBlast();
+        int travellerCount  = travellers.size();
+    	for (int i = 0; i < travellerCount; i++)
+    		travellers.get(0).hitByBlast();
     	for (int i = 0; i < neighbours.size(); i++)
     		neighbours.get(i).removeNeighbour(this);
     	sun.removeAsteroid(this);
